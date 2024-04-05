1b77a55f1088d3189ef0c768ead27ac2d9fc10ff
AndresJimenezS <andresjsolis@gmail.com>
2024-04-04 19:12:15 -0600

    Implementación código funcional ingreso de nuevos productos. Vista productos admin.

---
+++ js/productosAdmin.js
@@ -0,0 +1,53 @@
+
+$(document).ready(function () {
+    cargaProductos();
+})
+
+// Función AJAX productos
+function cargaProductos() {
+    try{
+        $.ajax({
+            url: 'DAL/productosAdmin.php'
+        })
+            .done(function (data){
+                ProductosJson(data);
+            });
+    
+    }catch (err) {
+        alert(err);
+    }
+}
+
+
+
+// Función ingresa registros de productos
+function ProductosJson(TextoJSON) {
+  
+    var ObjetoJSON = JSON.parse(TextoJSON);
+
+    var html = ""; 
+
+    for (var i = 0; i < ObjetoJSON.length; i++) {
+        // genera HTML dinámicamente
+        html += "<tr class='w-100'>"
+        html += "<td>" + i + "</td>"
+        html += "<td>"  + ObjetoJSON[i].nombre + "</td>"
+        html += "<td>"  + ObjetoJSON[i].precio + "</td>"
+        html += "<td>"  + ObjetoJSON[i].existencias + "</td>"
+        html += "<td>"  + ObjetoJSON[i].peso + "</td>"
+        html += "<td>";
+        if (ObjetoJSON[i].activo != 0) {
+            html += "Sí";
+        } else {
+            html += "No";
+        }
+        html += "</td>";
+        html += '<td><a id="idProducto" href="productoDetalle.php?idProducto=' + ObjetoJSON[i].id_producto + '" class="text-primary text-decoration-none">Editar</a></td>';
+
+        html += "</tr>"
+    }
+
+    // agregar html al div productos
+    $("#tabla #fila").append(html);
+}
+
